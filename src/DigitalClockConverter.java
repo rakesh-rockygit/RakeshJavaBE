7b1302e61bf6c7c73ec5ab14d2dd4c1651329a87
Rakesh <rakeshresume99@gmail.com>
2026-04-17 15:22:30 -0700

    Digital Clock Conv

---
+++ src/DigitalClockConverter.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DigitalClockConverter {
+    // public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter seconds:");
+        // int secs = get.totalseconds();
+        String input = reader.readLine();
+        int seconds = Integer.parseInt(input);
+
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600);
+        int second = seconds % 60;
+
+        System.out.printf("%d:%02d:%02d", hours, minutes, second);
+
+    }
+
+}
