657886b138699c3ec44961fc022c131cebc25018
Rakesh <rakeshresume99@gmail.com>
2026-04-16 18:13:01 -0700

    multiple leap  years

--- src/LeapYearCheck.java
+++ src/LeapYearCheck.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class LeapYearCheck {
     public static void main(String[] args) {
 
-        int year = 2024;
+        Scanner scanner = new Scanner(System.in);
 
-        boolean Leapyear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Iteration " + i + ": Enter a year to check: ");
+            int year = scanner.nextInt();
 
-        System.out.println(year + "\n--is a leap year\n" + Leapyear);
+            boolean Leapyear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
+            System.out.println(year + "\n--is a leap year\n" + Leapyear);
+            scanner.close();
+        }
     }
 }
