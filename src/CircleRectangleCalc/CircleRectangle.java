2dd0998ab3bbd0c6fda85bc9c5d0df69f81806b3
Rakesh <rakeshresume99@gmail.com>
2026-04-20 14:14:36 -0700

    Circle and Rectangle Cal

--- src/CircleRectangleCalc/CircleRectangle.java
+++ src/CircleRectangleCalc/CircleRectangle.java
@@ -7,13 +7,25 @@ public class CircleRectangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter radius: ");
-        // System.out.print("Enter width: ");
-        // System.out.print("Enter height: ");
+        System.out.print("\nEnter radius: ");
 
         double radius = scanner.nextDouble();
         double area = Math.PI * radius * radius;
+        double circumference = 2 * Math.PI * radius;
+        double perimeter = 2 * Math.PI * radius;
+
         System.out.printf("Area of Circle: %.2f%n", area);
+        System.out.printf("Circumference: %.2f%n", circumference);
+        System.out.printf("perimeter: %.2f%n", perimeter);
+
+        System.out.print("\nEnter width: ");
+        double width = scanner.nextDouble();
+        System.out.print("\nEnter height: ");
+        double height = scanner.nextDouble();
+
+        double rectanglearea = height * width;
+
+        System.out.printf("area of rectangle: %.2f%n", rectanglearea);
 
     }
 }
