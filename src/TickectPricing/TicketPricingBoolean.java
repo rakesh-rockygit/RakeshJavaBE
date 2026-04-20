c1beb84d9bffc65a8910bea3d1ac8bb319378d1c
Rakesh <rakeshresume99@gmail.com>
2026-04-19 19:47:50 -0700

    Ticket Pricing Ternary

--- src/TickectPricing/TicketPricingBoolean.java
+++ src/TickectPricing/TicketPricingBoolean.java
@@ -7,14 +7,17 @@ import java.util.Scanner;
 //  Print all four booleans and a price: full price = 100, senior/non-member discount = 50, under 18 = 30.
 //Hint: Introduce the ternary operator: int price = fullPrice ? 100 : (isSenior ? 50 : 30).This previews conditionals
 
+//class 
 public class TicketPricingBoolean {
 
+    // static variable
     public static void main(String[] args) {
 
-        // int fullPrice = 100;
         // int age = 19;
+        // Instance Object
         Scanner scan = new Scanner(System.in);
 
+        // Instance variable
         System.out.print("Please enter your age: ");
         int age = scan.nextInt();
 
@@ -23,15 +26,16 @@ public class TicketPricingBoolean {
         boolean isSenior = age >= 65;
 
         boolean fullPrice = isAdult && isMember && !isSenior;
-
+        // ternary operator
         int price = fullPrice ? 100 : (isSenior ? 50 : 30);
 
+        // method call
         System.out.println("isMember :" + isMember);
         System.out.println("isAdult :" + isAdult);
         System.out.println("isSenior :" + isSenior);
         System.out.println("fullPrice :" + fullPrice);
 
         System.out.println("price :" + price);
-        scan.close();
+        // scan.close();
     }
 }
