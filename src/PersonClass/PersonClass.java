706bdda364fc0152581307c00f8a039eb17a8fa6
Rakesh <rakeshresume99@gmail.com>
2026-04-30 07:14:58 -0700

    Person Class demo

---
+++ src/PersonClass/PersonClass.java
@@ -0,0 +1,27 @@
+package PersonClass;
+
+public class PersonClass {
+
+    String name;
+    int age;
+    String city;
+
+    public PersonClass(String name, int age, String city) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+        System.out.printf("Hi, I'm %s, I'm %d years old and I live in %s." + name, age, city);
+
+    }
+
+    public void greet() {
+
+        System.out.printf("Hi, I'm %s, I'm %d years old and I live in %s." + name, age, city);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        PersonClass person1 = new PersonClass("rocky", 30, "vancouver");
+        person1.greet();
+    }
+}
