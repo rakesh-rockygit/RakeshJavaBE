c366850b406f9f56df83a406660960c8d5833cc9
Rakesh <rakeshresume99@gmail.com>
2026-05-02 11:10:54 -0700

    Rectangle Class summary

--- src/RectangleClass/RectangleClass.java
+++ src/RectangleClass/RectangleClass.java
@@ -4,9 +4,9 @@ public class RectangleClass {
 
     double width;
     double height;
-    double area;
-    double perimeter;
-    boolean isSquare;
+    // double area;
+    // double perimeter;
+    // boolean isSquare;
 
     public RectangleClass(double width, double height) {
 
@@ -18,34 +18,38 @@ public class RectangleClass {
 
     }
 
-    public double area(double width, double height) {
+    public double area() {
 
         return (this.width * this.height);
+
     }
 
-    public double perimeter(double width, double height) {
+    public double perimeter() {
         return 2 * (this.width + this.height);
     }
 
-    public boolean isSquare(double width, double height) {
+    public boolean isSquare() {
         return (this.width == this.height);
     }
 
     public String describe() {
-        return area + " " + perimeter + " " + isSquare + " ";
+        return "Area: " + area() +
+                ", Perimeter: " + perimeter() +
+                ", Is Square: " + isSquare();
     }
 
     public static void main(String[] args) {
 
-        RectangleClass r1 = new RectangleClass(20, 15);
-        RectangleClass r2 = new RectangleClass(20, 20);
-        RectangleClass r3 = new RectangleClass(30, 10);
-        System.out.println(r2.perimeter);
-        System.out.println(r2.describe());
+        RectangleClass r1 = new RectangleClass(20.00, 15.00);
+        RectangleClass r2 = new RectangleClass(30.00, 15.00);
+        RectangleClass r3 = new RectangleClass(40.00, 40.00);
 
-        r1.describe();
-        r2.describe();
-        r3.describe();
+        System.out.println(r1.area());
+        System.out.println(r1.perimeter());
+        System.out.println(r1.isSquare());
+        System.out.println(r1.describe());
+        System.out.println(r2.describe());
+        System.out.println(r3.describe());
 
     }
 
