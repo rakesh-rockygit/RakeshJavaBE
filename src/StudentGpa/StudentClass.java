d09e46dfc0ebe6d05c15752fc19dcab2dab03b92
Rakesh <rakeshresume99@gmail.com>
2026-05-03 13:30:08 -0700

    Student Class NewGpa

--- src/StudentGpa/StudentClass.java
+++ src/StudentGpa/StudentClass.java
@@ -5,12 +5,11 @@ public class StudentClass {
     String name;
     String studentid;
     double gpa;
-    double newGpa;
 
-    public StudentClass(String name, String studentid, double gpa, double newGpa) {
+    public StudentClass(String name, String studentid, double gpa) {
         this.name = name;
         this.studentid = studentid;
-        this.gpa = newGpa;
+        this.gpa = gpa;
 
     }
 
@@ -25,26 +24,28 @@ public class StudentClass {
 
     public void updateGpa(double newGpa) {
         this.gpa = newGpa;
-        // System.out.println( "new gpa is :" + updateGpa);
+        // System.out.println("new gpa is :" + newGpa);
     }
 
     public String displayCard() {
-        return " student id: " + studentid +
+        return " student name :" + name +
+                " student id: " + studentid +
                 " GPA is : " + gpa + "status: " + getStatus();
         // " Student Status : " + getStatus;
     }
 
     public static void main(String[] args) {
 
-        StudentClass SG1 = new StudentClass("rakesh", "1099", 8.9, 9.3);
-        StudentClass SG2 = new StudentClass("rahul", "1877", 6.9, 6.3);
-        StudentClass SG3 = new StudentClass("vivek", "1923", 4.9, 6.2);
+        StudentClass SG1 = new StudentClass("rakesh", "1099", 8.9);
+        StudentClass SG2 = new StudentClass("rahul", "1877", 6.9);
+        StudentClass SG3 = new StudentClass("vivek", "1923", 3.0);
 
         System.out.println(SG1.displayCard());
         System.out.println(SG2.displayCard());
         System.out.println(SG3.displayCard());
 
-        System.out.println(SG3.newGpa);
+        SG3.updateGpa(3.7);
+        System.out.println(SG3.displayCard());
 
         // SG1.studentid();
         // System.out.println(displayCard());
