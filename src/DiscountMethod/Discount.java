dfd6c1338a133d8732e815d363348e016bd6eb35
Rakesh <rakeshresume99@gmail.com>
2026-05-10 20:16:33 -0700

    Discount applied

--- src/DiscountMethod/Discount.java
+++ src/DiscountMethod/Discount.java
@@ -5,13 +5,44 @@ public class Discount {
     String name;
     String category;
     double price;
-}
 
-public Discount( int stockCount,
-    String name,
-    String category,
-    double price){
+    public Discount(int stockCount, String name, String category, double price) {
+        this.stockCount = stockCount;
+        this.name = name;
+        this.category = category;
+        this.price = price;
 
-    this.stockCount = stockCount;
+    }
 
+    public void applyDiscount(double percent) {
+        this.price = this.price - (this.price * percent / 100.0);
+    }
+
+    public void restock(int quantity) {
+        this.stockCount += quantity;
+    }
+
+    public String sell(int quantity) {
+        if (quantity <= this.stockCount) {
+            this.stockCount -= quantity;
+            return "stock available";
+        } else
+
+        {
+            return "error : no stock";
+        }
+    }
+
+    public String displayInfo() {
+        return "stock available is " + stockCount +
+                "name is" + name +
+                "category type" + category +
+                "price of item" + price;
+    }
+
+    public static void main(String[] args) {
+        Discount dm1 = new Discount(100, "kiwi", "fruit", 1500);
+        dm1.sell(10);
+        System.out.println(dm1.displayInfo());
+    }
 }
