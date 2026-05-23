8f04f1d8a0ac14bc3c2c871e9da3ece48fee2e1a
Rakesh <rakeshresume99@gmail.com>
2026-05-23 12:50:14 -0700

    perosn modify

--- src/PersonClass/PersonClass.java
+++ src/PersonClass/PersonClass.java
@@ -17,11 +17,13 @@ public class PersonClass {
     public void greet() {
 
         System.out.printf("Hi, I'm %s, I'm %d years old and I live in %s." + name, age, city);
-        System.out.println();
+
     }
 
     public static void main(String[] args) {
         PersonClass person1 = new PersonClass("rocky", 30, "vancouver");
+        PersonClass person2 = new PersonClass("rakesh", 2, "toronto");
         person1.greet();
+        person2.greet();
     }
 }
