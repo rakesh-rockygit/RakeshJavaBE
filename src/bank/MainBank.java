4c4b895a8c26461a4d5402ca831ae816e92e5aa2
Rakesh <rakeshresume99@gmail.com>
2026-05-25 12:57:18 -0700

    deposit withdraw returns

--- src/bank/MainBank.java
+++ src/bank/MainBank.java
@@ -7,9 +7,9 @@ public class MainBank {
     public static void main(String[] args) {
 
         BankingOperations bOperations = new BankingOperations();
-        System.out.println(bOperations.getBalance(985));
-        System.out.println(bOperations.deposit(9850, 9000));
-        System.out.println(bOperations.withdraw(98, 5000));
+        System.out.println("AccountBalance " + bOperations.getBalance(985));
+        System.out.println("Deposit " + bOperations.deposit(9850, 9000));
+        System.out.println("Withdraw " + bOperations.withdraw(98, 5000));
     }
 
 }
