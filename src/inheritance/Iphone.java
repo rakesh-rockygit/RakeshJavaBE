1551fe5b2d6e2bc0dc811c1587ea99d5dd085d5e
Rakesh <rakeshresume99@gmail.com>
2026-05-29 13:57:23 -0700

    POJO hashcode examnple 2 EQUALS HASCODE

--- src/inheritance/Iphone.java
+++ src/inheritance/Iphone.java
@@ -5,5 +5,13 @@ public static void main(String[] args) {
     Mobile mobile = new Mobile(); 
     int hashCode = mobile.hashCode();
 System.out.println("hashcode of the mobile object :" + hashCode);
+
+   // 
+    int hashCode1 = mobile.hashCode();
+System.out.println("hashcode of the mobile object :" + hashCode1);
+
+Mobile mobile1 = new Mobile(); 
+System.out.println("hashcode of the mobile object :" + mobile1.hashCode());
+
 }
 }
