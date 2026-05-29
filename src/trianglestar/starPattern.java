c63d5990aa663b0add998ca5058a57ba0463c084
Rakesh <rakeshresume99@gmail.com>
2026-05-29 11:03:21 -0700

    star Triangle row cloumn

--- src/trianglestar/starPattern.java
+++ src/trianglestar/starPattern.java
@@ -2,16 +2,17 @@ package trianglestar;
 
 public class starPattern {
 
-    private void printRightAngledTriangleStarPatterntarPattern(){
+    private void printRightAngledTriangleStarPattern(){
         for (int row = 1; row <= 4; row++){
             for (int column = 1; column <= row; column++){
                 System.out.print("*");
             }
+               System.out.println();
         }
     }
 
     public static void main(String[] args) {
         starPattern conditionalExamples = new starPattern();
-        //starpattern.printRightAngledTriangleStarPattern();
+        conditionalExamples.printRightAngledTriangleStarPattern();
     }
 }
