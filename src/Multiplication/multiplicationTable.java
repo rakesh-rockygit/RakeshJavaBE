f45dccfe1e087ac571f4f737b60215e250d2ca84
Rakesh <rakeshresume99@gmail.com>
2026-05-30 12:51:40 -0700

    multiply for loop

---
+++ src/Multiplication/multiplicationTable.java
@@ -0,0 +1,31 @@
+package Multiplication;
+
+public class multiplicationTable {
+
+   //  Ask the user for a number (int). 
+    // Print the full multiplication table for that number from 1 to 12. 
+    // Format each line as: '5 x 1 = 5', '5 x 2 = 10', etc.
+
+public void multiply(int n){
+
+
+
+    for (int i = 1; i <= 12; i++){
+    
+    System.out.println(n + " * " + i + " = " + (n * i));
+
+   //System.out.println("5 table : " + multiply(5));
+   //return n;
+
+}
+}
+
+
+public static void main(String[] args) {
+    multiplicationTable multiplicationtable = new multiplicationTable();
+    multiplicationtable.multiply(5);
+   // System.out.println(n + " * " + i + " = " + (n * i));
+  
+}
+}
+
