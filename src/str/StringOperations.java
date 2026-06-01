8641e13ea55ef278b154b71309cb344ed7058236
Rakesh <rakeshresume99@gmail.com>
2026-05-31 18:28:33 -0700

    string == and equals() differ

---
+++ src/str/StringOperations.java
@@ -0,0 +1,23 @@
+package str;
+
+public class StringOperations {
+
+    public static void main(String[] args) {
+        
+        String a = "world";
+        String b = "world";
+        String c = new String("world");
+
+        System.out.println(a.hashCode());
+        System.out.println(c.hashCode());
+
+// operator compares only references
+
+        System.out.println(a == b);
+        System.out.println(a == c);
+
+        //equals method compares references and numbers, values
+        System.out.println(a.equals(c));
+
+    }
+}
