363b3dd64091c3bab6a55522c41ebccdf872c26f
Rakesh <rakeshresume99@gmail.com>
2026-06-13 06:43:04 -0700

    arrayList example

---
+++ src/collections/arrayList.java
@@ -0,0 +1,24 @@
+package collections;
+
+public class arrayList {
+
+    public static void main(String[] args) {
+        arrayList arraylist = new arrayList();
+    }
+    void arrayListmethods() {
+// adding items to array list
+        arrayList<String>  arraylist = new arrayList<>(initialCapacity: 4);
+
+        cityList.add("city1");
+        cityList.add("city2");
+        cityList.add("city3");
+
+        for (String city : cityList){
+            if (cityList.contains("3")){
+                System.out.println(city.toUpperCase());
+            }
+            System.out.println();
+             System.out.println(cityList);
+        }
+    }
+}
