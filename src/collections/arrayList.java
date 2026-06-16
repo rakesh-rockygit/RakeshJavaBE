f06a09738ed8fa922d88e27d2ce630cc72f12283
Rakesh <rakeshresume99@gmail.com>
2026-06-16 05:35:02 -0700

    List set get sort example

--- src/collections/arrayList.java
+++ src/collections/arrayList.java
@@ -12,13 +12,39 @@ public class arrayList {
         cityList.add("city1");
         cityList.add("city2");
         cityList.add("city3");
+        cityList.add("city4");
+        cityList.add("city5");
+
+        cityList.add(index: 3, element: "city3");
+
+        System.out.println(cityList.contains("city3"));
+        if (cityList.contains("city3"))
+            System.out.println(cityList.get(3).toUpperCase());
+
+        System.out.println(cityList.get(3));
 
         for (String city : cityList){
             if (cityList.contains("3")){
                 System.out.println(city.toUpperCase());
-            }
-            System.out.println();
-             System.out.println(cityList);
+
+                arraylist<Integer> intList = new arrayList<>();
+                intList.add(5);
+                intList.add(3);
+
+        intList.sort{
+            (c1, c2) -> {
+        if(c1 < c2) return -1; return 0;
+            };
+                
+    cityList.sort(c1, c2) -> {
+        if(c1.equals(c2)) return -1; return 0;
+            });
+    
+        for(String city : cityList) {
+             System.out.println(city);
         }
+
+         System.out.println();
+          System.out.println(cityList);
     }
-}
+    }
