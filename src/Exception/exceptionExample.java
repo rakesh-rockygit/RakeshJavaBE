8cec2cfe57d5182277308fc3a5ac7f7db986c320
Rakesh <rakeshresume99@gmail.com>
2026-06-28 10:49:11 -0700

    Handling exception practice2

---
+++ src/Exception/exceptionExample.java
@@ -0,0 +1,17 @@
+package Exception;
+
+public class exceptionExample {
+    
+    public static void main(String[] args) {
+        try {
+            int data = 50 / 0;                        // Throws ArithmeticException
+            System.out.println("This text will not print.");
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero! " + e.getMessage());
+        } finally {
+            System.out.println("The finally block always executes.");
+        }
+        System.out.println("The rest of the program continues normally...");
+    }
+}
+
